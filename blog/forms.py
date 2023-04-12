33d50bf0c6cadf5a2fdfdb7c4cd992136633a5e7
Miracle <collinsalex50@gmail.com>
2023-04-12 23:45:50 +0100

    setup and blof

---
+++ blog/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+
+from blog.models import Post
+
+from ckeditor_uploader.widgets import CKEditorUploadingWidget
+
+
+class PostAdminForm(forms.ModelForm):
+    body = forms.CharField(widget=CKEditorUploadingWidget())
+    class Meta:
+        model = Post
+        fields = '__all__'
---
+++ blog/templatetags/custom_tags.py
@@ -0,0 +1,47 @@
+from django import template
+
+from taggit.models import Tag
+from listings.models import Listing
+
+
+
+register = template.Library()
+
+
+
+# get tags
+@register.simple_tag
+def get_tags(count=7):
+    # Gets all tags
+    return Tag.objects.all()[:count]
+
+
+@register.simple_tag
+def featured_listings(count=10):
+    # retrieves featured listings
+    listings = Listing.objects.filter(featured=True).filter(
+                    status=Listing.PUBLISHED).order_by('-created_at')[:count]
+    return listings
+
+
+@register.filter
+def shrink(value, num_decimals=2):
+    """
+    Django template filter to convert regular numbers to a
+    cool format (ie: 2K, 434.4K, 33M...)
+    :param value: number
+    :param num_decimals: Number of decimal digits
+    """
+    try:
+        int_value = int(value)
+    except:
+        return
+            
+    formatted_number = '{{:.{}f}}'.format(num_decimals)
+    if int_value < 1000:
+        return str(int_value)
+    elif int_value < 1000000:
+        return formatted_number.format(int_value/1000.0).rstrip('0').rstrip('.')+ 'K'
+    else:
+        return formatted_number.format(int_value/1000000.0).rstrip('0').rstrip('.') + 'M'
+    
