bce4a706b7f6a0efd0ebf5f107bf1795b3635d12
Miracle <collinsalex50@gmail.com>
2023-04-21 15:49:02 +0100

    added static files setup

--- mysite/settings.py
+++ mysite/settings.py
@@ -129,3 +129,9 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = "users.User"
+
+STATIC_ROOT = BASE_DIR / 'static'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'mysite/static',
+]
